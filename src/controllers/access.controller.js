d02e37a150e0c3a32943c20d1749141c96d8837b
HoangBluecodedao <dongthanhquandtq@gmail.com>
2025-04-25 12:37:56 +0700

    api response use class

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,11 +1,16 @@
 'use strict'
 
+const { OK, CREATED } = require('../core/success.response')
 const AccessService = require('../services/access.service')
-
 class AccessController {
-
     signUp = async ( req, res, next ) => {
-        return res.status(201).json(await AccessService.signUp(req.body))
+        new CREATED({
+            message: 'Regiserted OK!',
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limit: 10
+            }
+        }).send(res)
     }
 }
 
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -18,55 +18,53 @@ class AccessService {
 
     static signUp = async({name, email, password}) => {
 
-        // try {
+        // step 1: check if email exists
+        const holderShop = await shopModel.findOne({email}).lean() // lean for faster search
 
-            // step 1: check if email exists
-            const holderShop = await shopModel.findOne({email}).lean() // lean for faster search
+        if (holderShop) {
+            throw new BadRequestError('Error: Shop already registered!')
+        }
 
-            if (holderShop) {
-                throw new BadRequestError('Error: Shop already registered!')
-            }
+        const passwordHash = await bcrypt.hash(password, 10)
 
-            const passwordHash = await bcrypt.hash(password, 10)
-
-            const newShop = await shopModel.create({
-                name, email, password: passwordHash, roles: [RoleShop.SHOP]
-            })
+        const newShop = await shopModel.create({
+            name, email, password: passwordHash, roles: [RoleShop.SHOP]
+        })
+        
+        if (newShop) {
+            // create privateKey, publicKey
+            const privateKey = crypto.randomBytes(64).toString('hex')
+            const publicKey = crypto.randomBytes(64).toString('hex')
             
-            if (newShop) {
-                // create privateKey, publicKey
-                const privateKey = crypto.randomBytes(64).toString('hex')
-                const publicKey = crypto.randomBytes(64).toString('hex')
-                
-                console.log({ privateKey, publicKey })
+            console.log({ privateKey, publicKey })
 
-                const keyStore = await KeyTokenService.createKeyToken({
-                    userId: newShop._id,
-                    publicKey,
-                    privateKey
-                })
+            const keyStore = await KeyTokenService.createKeyToken({
+                userId: newShop._id,
+                publicKey,
+                privateKey
+            })
 
-                if (!keyStore) {
-                    throw new BadRequestError('Error: Keystore already registered!')
-                }
-
-                // create token pair 
-                const tokens = await createTokenPair({userId: newShop._id, email},  publicKey, privateKey)
-                console.log(`Create tokens success::`, tokens)
-
-                return {
-                    code: 201,
-                    metadata: {
-                        shop: getInfoData({ fields: ['_id', 'name', 'email'], object: newShop}),
-                        tokens
-                    }
-                }
+            if (!keyStore) {
+                throw new BadRequestError('Error: Keystore already registered!')
             }
+
+            // create token pair 
+            const tokens = await createTokenPair({userId: newShop._id, email},  publicKey, privateKey)
+            console.log(`Create tokens success::`, tokens)
 
             return {
-                code: 200,
-                metadata: null
+                code: 201,
+                metadata: {
+                    shop: getInfoData({ fields: ['_id', 'name', 'email'], object: newShop}),
+                    tokens
+                }
             }
+        }
+
+        return {
+            code: 200,
+            metadata: null
+        }
     }
 }
 
